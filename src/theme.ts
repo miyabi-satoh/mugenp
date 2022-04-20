b7231a12e4dac115c104e020ba771a74baefae72
miyabi-satoh <miyabi.satoh@gmail.com>
2022-04-21 04:31:07 +0900

    とりあえず！

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,50 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import {
+  extendTheme,
+  withDefaultSize,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "light",
   useSystemColorMode: false,
 };
 
-export const theme = extendTheme({ config });
+const fontFamilies = [
+  // "-apple-system",
+  // "BlinkMacSystemFont",
+  // `"Segoe UI"`,
+  "Helvetica Neue",
+  "Helvetica",
+  "Arial",
+  "Meiryo",
+  `"Hiragino Sans"`,
+  `"Hiragino Kaku Gothic ProN"`,
+  "sans-serif",
+  `"Apple Color Emoji"`,
+  `"Segoe UI Emoji"`,
+  `"Segoe UI Symbol"`,
+];
+
+const monoFontFamilies = [
+  "SFMono-Regular",
+  "Menlo",
+  "Monaco",
+  "Consolas",
+  `"Liberation Mono"`,
+  `"Courier New"`,
+  "monospace",
+];
+
+const fonts = {
+  heading: fontFamilies.join(","),
+  body: fontFamilies.join(","),
+  mono: monoFontFamilies.join(","),
+};
+
+export const theme = extendTheme(
+  { config, fonts },
+  withDefaultSize({
+    size: "sm",
+    components: ["Input", "NumberInput", "Select", "Button"],
+  })
+);
