55d0e4eb0ff25ed57904ad760cdd20c915b2f309
miyabi-satoh <miyabi.satoh@gmail.com>
2022-04-27 04:25:55 +0900

    更新

--- src/pages/9-1-4-01.tsx
+++ src/pages/9-1-4-01.tsx
@@ -1,9 +1,12 @@
 import { MugenContainer } from "~/components/container";
-import { waseki_no_kousiki } from "~/core/09/01/02/waseki_no_kousiki";
+import { kyoutuu_bunkai } from "~/core/09/01/04/kyoutuu_bunkai";
 
 const Mugen = () => {
   return (
-    <MugenContainer title="Now construction" onRefresh={waseki_no_kousiki} />
+    <MugenContainer
+      title="次の式を因数分解しなさい。"
+      onRefresh={kyoutuu_bunkai}
+    />
   );
 };
 
