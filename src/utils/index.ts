2a863ef9fc6c0c58c70b7ba457aaed27d15b503a
miyabi-satoh <miyabi.satoh@gmail.com>
2022-04-27 02:44:10 +0900

    getRandomIntの変更

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,16 @@
 import { Fraction } from "./fraction";
 
-export function getRandomInt(max: number): number {
-  return Math.floor(Math.random() * max);
+export function getRandomInt(max: number, min: number = 0): number {
+  if (min > max) {
+    [max, min] = [min, max];
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function ifUnder<T1, T2>(percentage: number, v1: T1, v2: T2): T1 | T2 {
-  return getRandomInt(100) < percentage ? v1 : v2;
+  return getRandomInt(100) <= percentage ? v1 : v2;
 }
 
 export function checkParam(p: Fraction): boolean {
