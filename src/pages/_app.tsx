ffc22afd31ded7efdf125587974fb87359fd8f58
miyabi-satoh <miyabi.satoh@gmail.com>
2022-05-15 17:53:44 +0900

    GTM設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
-// import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "~/theme";
 import { MathJaxContext } from "better-react-mathjax";
 import { DefaultSeo } from "next-seo";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-MVKQTLQ" });
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <MathJaxContext>
