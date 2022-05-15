65a7d4b6112123336cacd1342ef6af05e09698fc
miyabi-satoh <miyabi.satoh@gmail.com>
2022-05-15 13:54:41 +0900

    構造を整理

--- src/core/g08/c01/s01/index.ts
+++ src/core/g08/c01/s01/index.ts
@@ -1,5 +1,26 @@
-export * from "./01";
+// 1節　式の計算
 
-export * from "./02";
+// 1項 式の加法，減法
+// ・文字の式に関連した用語の意味
+//     単項式、多項式、項、次数、一次式、二次式、同類項
+// ・同類項の意味と同類項をまとめること
+// ・多項式の加法，減法
+export * from "./M81111_kou_jisuu";
+export * from "./M81112_douruikou_no_seiri";
+export * from "./M81113_kahou_genpou";
 
-export * from "./03";
+// 2項 いろいろな多項式の計算
+// ・多項式と数の乗法，除法
+// ・(数×多項式) の加減
+// ・式を計算してから代入して，式の値を求めること
+export * from "./M81121_poly_num_mul";
+export * from "./M81122_poly_num_div";
+export * from "./M81123_bunpai_kagen";
+export * from "./M81124_bunsuu_bunpai_kagen";
+export * from "./M81125_bunsuukei_kagen";
+
+// 3項 単項式の乗法，除法
+// ・単項式どうしの乗法，除法
+// ・乗除の混じった単項式の計算
+export * from "./M81131_mono_mono_mul";
+export * from "./M81132_mono_mono_div";
