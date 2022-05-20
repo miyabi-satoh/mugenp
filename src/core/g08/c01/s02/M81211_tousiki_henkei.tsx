427b06a19da6e23465d1a7828fdcac7def8b6390
miyabi-satoh <miyabi.satoh@gmail.com>
2022-05-20 13:18:45 +0900

    作業中

--- src/core/g08/c01/s02/M81211_tousiki_henkei.tsx
+++ src/core/g08/c01/s02/M81211_tousiki_henkei.tsx
@@ -1,6 +1,6 @@
 import { MugenContainer } from "~/components/container";
 import { RefreshFunction } from "~/interfaces/types";
-import { dsp, getRandomInt, guard } from "~/utils";
+import { dsp, getRandomInt, guard, randArray } from "~/utils";
 import { Monomial } from "~/utils/monomial";
 import { Polynomial } from "~/utils/polynomial";
 
@@ -20,52 +20,30 @@ export { Mugen as M81211 };
 
 // 等式変形
 const handleRefresh: RefreshFunction = (level, score) => {
-  // Lv1: 左辺→右辺へ移項
-  // Lv2: 左辺→右辺、右辺→左辺へそれぞれ移項
-  // Lv3: 移項後、自然数で割る(約分なし)
-  // Lv4: 移項後、整数で割る(約分もあり)
-  // Lv5: 移項後、文字で割る(文字の約分はなし)
-  // Lv6: 多項式と数の積商を含む
   const idx = level - 1;
-  let a = Monomial.create({
-    max: guard(idx, 1, 1, 9),
-    factors: "x",
+  const terms: Monomial[] = [];
+  ["a", "b", "c", ""].forEach((c, i) => {
+    const x = Monomial.create({
+      max: level + i, // level + (0,1,2,3)
+      maxD: guard(idx, 1, 1, 1, 5),
+      maxN: guard(idx, 1, 1, 1, 5),
+      factors: c,
+      allowNegative: true,
+      allowZero: true,
+    });
+    terms.push(x);
   });
-  let b = Monomial.create({
-    max: 9,
-    factors: "y",
-    allowNegative: true,
-  });
-  let c = Monomial.create({
-    max: 9,
-    allowNegative: true,
-    allowZero: true,
-  });
+
+  const pLeft = new Polynomial(
+    ...terms.splice(0, getRandomInt(terms.length - 1, 1))
+  );
+  const pRight = new Polynomial(...terms);
 
   let question = "";
   let answer = "";
-  if (level === 1) {
-    question =
-      a.toLatex() +
-      b.toLatex({ sign: true }) +
-      "=" +
-      c.toLatex() +
-      "\\quad [" +
-      a.factors[0] +
-      "]";
-    answer = a.toLatex() + "=" + c.toLatex() + b.neg().toLatex({ sign: true });
-  }
-  if (level === 2) {
-    question =
-      b.toLatex() +
-      "=" +
-      a.neg().toLatex() +
-      c.toLatex({ sign: true }) +
-      "\\quad [" +
-      a.factors[0] +
-      "]";
-    answer = a.toLatex() + "=" + c.toLatex() + b.neg().toLatex({ sign: true });
-  }
+
+  question = pLeft.toLatex() + " = " + pRight.toLatex();
+  answer = question;
 
   return [dsp(question), dsp(answer)];
 };
