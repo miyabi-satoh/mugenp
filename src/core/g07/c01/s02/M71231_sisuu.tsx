80dda97c30324476e9b9b3edccfa592222d3cdc8
miyabi-satoh <miyabi.satoh@gmail.com>
2022-06-05 21:20:58 +0900

    コメントアウト部を除去

--- src/core/g07/c01/s02/M71231_sisuu.tsx
+++ src/core/g07/c01/s02/M71231_sisuu.tsx
@@ -85,32 +85,6 @@ const generatorFunc: GeneratorFunc = (level) => {
     }
   }
 
-  // const idx = level - 1;
-  // const limit = e > 2 ? 5 : 13;
-  // let x = Monomial.create({
-  //   max: guard(idx, 5, 5, limit),
-  //   maxD: guard(idx, 1, 1, 1, 5, limit),
-  //   maxN: guard(idx, 1, 1, 1, 5, limit),
-  //   allowNegative: guard(idx, false, true),
-  // });
-
-  // let decimal = guard(idx, false, false, false, false, randArray(true, false));
-  // if (decimal) {
-  //   if (e === 3) {
-  //     decimal = false;
-  //   } else if (x.d === 1) {
-  //     x = x.div(10);
-  //   } else if (!(x.d === 2 || x.d === 5 || x.d === 10)) {
-  //     decimal = false;
-  //   } else if (x.abs().compare(1.3) > 0) {
-  //     decimal = false;
-  //   }
-  // }
-
-  // let brackets = randArray("", "()");
-  // if (level <= 2 || (!decimal && x.isFrac)) {
-  //   brackets = "()";
-  // }
   const question = x.toLatex({ brackets, decimal }) + `^${e}`;
   let ans = x.pow(e);
   if (brackets == "") {
